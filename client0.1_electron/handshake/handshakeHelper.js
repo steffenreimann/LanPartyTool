ae0910cf3d5d26410a8c2eb2b63c9defa03e7a85
Jonas Ahlf <javerik.cyber@googlemail.com>
2019-01-30 23:01:20 +0100

    Added handshakeHelper for generating handshakeToken

---
+++ client0.1_electron/handshake/handshakeHelper.js
@@ -0,0 +1,22 @@
+const aes = require('./../utils/aesEncrypt');
+const uuid = require('uuid/v4');
+
+function generateToken(userUUID) {
+    const tokenUUID = uuid();
+    return aes.EncryptBuffer(userUUID, Buffer.from(tokenUUID, 'utf8'));
+}
+
+function decryptToken(userUUID, token, isString) {
+    let decrypted = '';
+    if (isString === undefined) {
+        decrypted = aes.DecryptBuffer(userUUID, token);
+    } else {
+        decrypted = aes.DecryptBuffer(userUUID, Buffer.from(token));
+    }
+    return decrypted;
+}
+
+module.exports = {
+    GenerateToken: generateToken,
+    DecryptToken: decryptToken
+};
