8157e594c18d93dec2c84ccaba5f25a370d5f2d2
Javerik <javerik.cyber@googlemail.com>
2019-01-31 12:31:22 +0100

    added configPaths module to store all configFile names and creating paths to them

---
+++ client0.1_electron/config/configPaths.js
@@ -0,0 +1,50 @@
+const os = require('os');
+const fs = require('fs');
+
+
+// BaseDirectory for configuration files
+let homeDir = '';
+let baseDir = 'LanToolConfigs';
+
+// Static config files
+const userCfgName = 'user.cfg';
+const friendListName = 'friends.list';
+
+// Config-file paths
+let userCfgPath = null;
+let friendListPath = null;
+
+/**
+ * Generates paths to config files
+ * @return {boolean} BaseDir exists
+ */
+function init() {
+    // Set the baseDirectory
+    homeDir = os.homedir();
+    baseDir = path.join(homeDir, baseDir);
+
+    // Generate config file paths
+    userCfgPath = path.join(baseDir, userCfgName);
+    friendListPath = path.join(baseDir, friendListName);
+
+    return fs.existsSync(baseDir);
+}
+
+function getBaseDir() {
+    return baseDir;
+}
+
+function getUserCfgPath() {
+    return userCfgPath;
+}
+
+function getFriendListPath() {
+    return friendListPath;
+}
+
+module.exports = {
+    Init: init,
+    GetBaseDir: getBaseDir,
+    GetUserCfgPath: getUserCfgPath,
+    GetFriendListPath: getFriendListPath
+};
